bf377a2381e1783838cd4dc522cbc98207346254
sabarees48 <54196405+sabarees48@users.noreply.github.com>
2025-09-20 21:11:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 app.get('/', (req, res) => {
-res.send('Hello from Node.js app on EKS!!!');
+res.send('Hello from Node.js app on EKS!!! \nNew line for Testing!!!');
 });
 app.listen(port, () => {
 console.log(`App running at http://localhost:${port}`);
 
 });
+
